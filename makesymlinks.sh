e034b8289dc221c533094d02d1bb7c9994955832
jtpereyda <jtpereyda@users.noreply.github.com>
2016-05-10 16:56:04 -0700

    Use mv -b for safer backup copying

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -25,7 +25,7 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv -b ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
