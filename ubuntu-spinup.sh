07f28eae9833a8cb310aa73680ad8453ba0f9fc9
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2017-10-26 14:46:22 -0700

    add package for tmux.conf workaround

--- ubuntu-spinup.sh
+++ ubuntu-spinup.sh
@@ -1,2 +1,7 @@
 sudo apt update --assume-yes && sudo apt upgrade --assume-yes
+
+# dev tools
 sudo apt install --assume-yes tmux vim wireshark git
+
+# bc is needed for a workaround in dotfiles/tmux.conf
+sudo apt install --assume-yes bc
