47bea40ba79e04397bc78337e33aa8c07c8da988
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2017-10-31 21:01:59 -0700

    Add vim Pathogen and vim-better-whitespace plugin

--- ubuntu-spinup.sh
+++ ubuntu-spinup.sh
@@ -5,3 +5,8 @@ sudo apt install --assume-yes tmux vim wireshark git
 
 # bc is needed for a workaround in dotfiles/tmux.conf
 sudo apt install --assume-yes bc
+
+# clone some vim plugins
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/ntpeters/vim-better-whitespace.git ~/.vim/bundle/vim-better-whitespace
