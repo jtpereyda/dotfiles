2af88f16a2871de906bdea0afacc24d4adbe68f0
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2018-05-03 17:39:46 -0700

    Add pull for vim plugin install to update on re-run

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # clone some vim plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle
+
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 git clone https://github.com/ntpeters/vim-better-whitespace.git ~/.vim/bundle/vim-better-whitespace
+git -C ~/.vim/bundle/vim-better-whitespace pull
