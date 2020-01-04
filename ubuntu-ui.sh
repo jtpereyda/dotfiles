e435110f9082eb06fc6ae68e626695d6ab397f2b
Joshua Pereyda <joshua.pereyda@oracle.com>
2020-01-04 14:27:18 -0800

    vimrc to solarized and enable termguicolors; konsole for i3 default term

---
+++ ubuntu-ui.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# scrot is for i3 screenshots
+sudo apt install --assume-yes scrot
--- ubuntu-vm.sh
+++ ubuntu-vm.sh
@@ -2,5 +2,6 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 $DIR/ubuntu.sh
+$DIR/ubuntu-ui.sh
 
 gsettings set org.gnome.desktop.session idle-delay 0  # don't lock VMs on idle
