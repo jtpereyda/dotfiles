dbdb4a4818ba7dd7ea15715e08f9ce98c4529c82
Joshua Pereyda <joshua.t.pereyda@gmail.com>
2020-08-15 11:59:47 -0700

    chocolatey -> winget

--- windows.bat
+++ windows.bat
@@ -1,1 +1,7 @@
-choco install googlechrome keepass dropbox git notepadplusplus cloudstation onenote microsoft-windows-terminal pycharm-community
+REM choco install googlechrome keepass dropbox git notepadplusplus cloudstation onenote microsoft-windows-terminal pycharm-community
+winget install git
+winget install googlechrome
+winget install keepass
+winget install dropbox
+winget install notepadplusplus
+winget install onenote
