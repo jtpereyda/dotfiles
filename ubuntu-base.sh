7f4cf1bf7ca9b14b61718e026935d18f65f13c99
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2021-02-23 10:57:32 -0800

    vim-gtk3

--- ubuntu-base.sh
+++ ubuntu-base.sh
@@ -4,7 +4,7 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 #sudo apt update --assume-yes && sudo apt upgrade --assume-yes
 
 # dev tools
-sudo apt install --assume-yes curl tmux vim git python python-pip
+sudo apt install --assume-yes curl tmux vim git python
 #sudo apt install --assume-yes wireshark
 
 # bc is needed for a workaround in dotfiles/tmux.conf
--- ubuntu-ui.sh
+++ ubuntu-ui.sh
@@ -2,4 +2,5 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # scrot is for i3 screenshots
-sudo apt install --assume-yes scrot
+# vim-gtk3 enables vim clipboard features
+sudo apt install --assume-yes scrot vim-gtk3
