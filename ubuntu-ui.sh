31afc9a7320a62b3e95668fcbbfee610ddc87c73
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2021-05-31 09:37:14 -0700

    volumeicon-alsa

--- ubuntu-ui.sh
+++ ubuntu-ui.sh
@@ -3,4 +3,5 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # scrot is for i3 screenshots
 # vim-gtk3 enables vim clipboard features
-sudo apt install --assume-yes scrot vim-gtk3
+# volumeicon-alsa for i3 volume icon
+sudo apt install --assume-yes scrot vim-gtk3 volumeicon-alsa
